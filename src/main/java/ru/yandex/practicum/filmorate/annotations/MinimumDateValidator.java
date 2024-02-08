a67f73f0e5d8c7928169317074fe318d3adc3249
Dan <dan.s.boltachev@yandex.ru>
2024-02-08 22:03:18 +0300

    feat: реализовать вэб приложение с базовыми инструментами Spring (Спринт 9) - реализовать REST API (GET, POST, PUT) - добавить валидацию аннотацией - добавить логирование и тестирование

--- src/main/java/ru/yandex/practicum/filmorate/annotations/MinimumDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/annotations/MinimumDateValidator.java
@@ -11,8 +11,9 @@ public class MinimumDateValidator implements ConstraintValidator<MinimumDate, Lo
     public void initialize(MinimumDate constraintAnnotation) {
         minimumDate = LocalDate.parse(constraintAnnotation.value());
     }
+
     @Override
     public boolean isValid(LocalDate value, ConstraintValidatorContext context) {
-        return value == null || !value.isBefore(minimumDate);
+        return !value.isBefore(minimumDate);
     }
 }
